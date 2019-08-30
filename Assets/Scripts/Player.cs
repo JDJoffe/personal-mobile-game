5814380c29139927115f2ff20bfebd691980d2e1
josh <48034855+JDJoffe@users.noreply.github.com>
2019-08-30 13:45:41 +1000

    shooting & enemycam

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Var")]
+    public bool canShoot = true;
+    public float shootTimer;
+    public float health;
+    [Header("Objects")]
+    public Camera mainCam;
+    public GameObject gun;
+    public Rigidbody bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region gun
+        float x = Screen.width / 2;
+        float y = Screen.height / 2;
+
+        var gunray = mainCam.ScreenPointToRay(new Vector3(x, y, 0));
+
+        shootTimer += Time.deltaTime;
+        if (shootTimer > 1f && canShoot == true && Input.GetKey(KeyCode.Mouse0))
+        {
+            Rigidbody clone = Instantiate(bulletPrefab, gun.transform.position, gun.transform.rotation);
+            var bulletray = mainCam.ScreenPointToRay(new Vector3(x, y, 0));
+
+            clone.velocity = bulletray.direction * 40;
+            shootTimer = 0;
+        }
+        #endregion
+    }
+}
