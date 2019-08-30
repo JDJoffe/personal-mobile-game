638761bf09b17bde4a67abd4e40392f92824bc39
josh <48034855+JDJoffe@users.noreply.github.com>
2019-08-30 14:26:29 +1000

    Update Player.cs
    gun points in weird direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,11 +11,12 @@ public class Player : MonoBehaviour
     [Header("Objects")]
     public Camera mainCam;
     public GameObject gun;
+    private GameObject barrel;
     public Rigidbody bulletPrefab;
     // Start is called before the first frame update
     void Start()
     {
-       
+        barrel = GameObject.Find("barrel");
        
     }
 
@@ -26,21 +27,35 @@ public class Player : MonoBehaviour
         float x = Screen.width / 2;
         float y = Screen.height / 2;
 
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.forward, out hit))
-        {
-            Vector3 hitPosition = hit.point;
-            gun.transform.LookAt(hitPosition);
-        }
+        
+      //  RaycastHit hit;
+        //fire a ray at centre of screen
+      //  if (Physics.Raycast(transform.position, Vector3.forward, out hit))
+      //  {
+           var gunray = mainCam.ScreenPointToRay(Vector3.forward);
+        gun.transform.LookAt(gunray.direction);
+      //  }
+
             shootTimer += Time.deltaTime;
+        //fire bullet from gun 
         if (shootTimer > 1f && canShoot == true && Input.GetKey(KeyCode.Mouse0))
         {
-            Rigidbody clone = Instantiate(bulletPrefab, gun.transform.position, gun.transform.rotation);
+            //instantiate clone at barrel position
+            Rigidbody clone = Instantiate(bulletPrefab, barrel.transform.position, barrel.transform.rotation);
+            //go to centre of screen
             var bulletray = mainCam.ScreenPointToRay(new Vector3(x, y, 0));
-
+            //clone speed and direction
             clone.velocity = bulletray.direction * 40;
             shootTimer = 0;
         }
         #endregion
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        var hit = mainCam.ScreenPointToRay(Vector3.forward);
+        Gizmos.DrawRay(hit);
+        Debug.DrawRay(gun.transform.position, Vector3.forward);
+      
     }
 }
