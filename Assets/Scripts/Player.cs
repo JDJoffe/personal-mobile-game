79d3864b3edfc1ccdf08d14c94830443698f5094
josh <48034855+JDJoffe@users.noreply.github.com>
2019-09-05 18:26:13 +1000

    player movement
    slides for some reason probs colliders or somesht

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,50 +8,79 @@ public class Player : MonoBehaviour
     public bool canShoot = true;
     public float shootTimer;
     public float health;
+    public float moveSpeed = 2f;
     [Header("Objects")]
     public Camera mainCam;
-    public GameObject gun;
-    private GameObject barrel;
+    public GameObject barrel;
     public Rigidbody bulletPrefab;
+    Rigidbody player;
     // Start is called before the first frame update
     void Start()
     {
         barrel = GameObject.Find("barrel");
-       
+        player = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        #region movement
+
+        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                player.velocity = transform.forward * moveSpeed;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                player.velocity = transform.right * -moveSpeed;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                player.velocity = transform.forward * -moveSpeed;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                player.velocity = transform.right * moveSpeed;
+            }
+        }
+
+        if (Application.platform == RuntimePlatform.IPhonePlayer || Application.platform == RuntimePlatform.Android)
+        {
+
+        }
+        #endregion
         #region gun
         float x = Screen.width / 2;
         float y = Screen.height / 2;
 
-        
-      //  RaycastHit hit;
+
+        //  RaycastHit hit;
         //fire a ray at centre of screen
-      //  if (Physics.Raycast(transform.position, Vector3.forward, out hit))
-      //  {
+        //  if (Physics.Raycast(transform.position, Vector3.forward, out hit))
+        //  {
         //   var gunray = mainCam.ScreenPointToRay(Vector3.forward);
         //gun.transform.LookAt(gunray.direction);
-      //  }
+        //  }
 
-            shootTimer += Time.deltaTime;
-        //fire bullet from gun 
+        shootTimer += Time.deltaTime;
+        // fire bullet from gun 
         if (shootTimer > 1f && canShoot == true && Input.GetKey(KeyCode.Mouse0))
         {
-            //instantiate clone at barrel position
-            Rigidbody clone = Instantiate(bulletPrefab, barrel.transform.position, barrel.transform.rotation);
+            // instantiate clone at barrel position
+            Vector3 endOfBarrel = new Vector3(0f, 0f, +.4f);
+            Rigidbody clone = Instantiate(bulletPrefab, barrel.transform.position + endOfBarrel, barrel.transform.rotation);
             //go to centre of screen
             var bulletray = mainCam.ScreenPointToRay(new Vector3(x, y, 0));
             //clone speed and direction
             clone.velocity = bulletray.direction * 40;
-            shootTimer = 0;
+            Destroy(clone.gameObject, 3f);
+            shootTimer = 0; // put new stuff above here so it actually runs dickhed          
         }
-        #endregion
-        #region turning
 
         #endregion
+
     }
     private void OnDrawGizmos()
     {
@@ -59,6 +88,6 @@ public class Player : MonoBehaviour
         //var hit = mainCam.ScreenPointToRay(Vector3.forward);
         //Gizmos.DrawRay(hit);
         //Debug.DrawRay(gun.transform.position, Vector3.forward);
-      
+
     }
 }
